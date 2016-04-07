a120e60d237077badf64648959d8041d9c52da7a
jensfinnas <jens.finnas@gmail.com>
2016-04-07 16:22:12 +0200

    Övningar med listor

---
+++ dag2/02_rakna_langa_namn.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+
+enhorningar = [
+    "Jens Finnäs",
+    "Leo Wallentin",
+    "Carl-Gustaf Lindén",
+    "Gustav Halth",
+    "Linus Lång",
+    "Ulf Fahlén"
+]
+
+langa_namn = 0
+korta_namn = 0
+
+for namn in enhorningar:
+	print(namn)
