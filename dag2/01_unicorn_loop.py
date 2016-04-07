4bfd5127d0a9ecd8250b6e4620c1ce5e45d1345a
jensfinnas <jens.finnas@gmail.com>
2016-04-07 13:21:43 +0200

    Complete names

--- dag2/01_unicorn_loop.py
+++ dag2/01_unicorn_loop.py
@@ -1,21 +1,13 @@
 # coding: utf-8
 
 enhorningar = [
-    "Jens",
-    "Leo",
-    "Carl-Gustaf",
-    "Gustav",
-    "Linus",
-    "Ulf"
+    "Jens Finnäs",
+    "Leo Wallentin",
+    "Carl-Gustaf Lindén",
+    "Gustav Halth",
+    "Linus Lång",
+    "Ulf Fahlén"
 ]
-
-"""
-ÖVNING:
-Gör en for-loop som skriver ut en numrerad deltagarlista.
-1. Jens
-2. Leo
-o.s.v.
-"""
 
 raknare = 1
 print("Nu börjar programmet! Vi sätter räknaren på 1.")
