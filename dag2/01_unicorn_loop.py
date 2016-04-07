610bfb9548e9962b51ca0a6432cbf14930fc78c8
jensfinnas <jens.finnas@gmail.com>
2016-04-07 13:18:20 +0200

    First commit

---
+++ dag2/01_unicorn_loop.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+
+enhorningar = [
+    "Jens",
+    "Leo",
+    "Carl-Gustaf",
+    "Gustav",
+    "Linus",
+    "Ulf"
+]
+
+"""
+ÖVNING:
+Gör en for-loop som skriver ut en numrerad deltagarlista.
+1. Jens
+2. Leo
+o.s.v.
+"""
+
+raknare = 1
+print("Nu börjar programmet! Vi sätter räknaren på 1.")
+
+
---
+++ hemuppgift1.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+
+def skriv_artikel(kommun, m, c, fp, kd, mp, s, v, sd, ovr, ogiltiga, ej_rost):
+    print "Skriv en rubrik och ingress här"
+
+
+skriv_artikel("Upplands Väsby", 6972, 919, 1501, 1093, 1680, 7428, 1430, 2679, 825, 236, 4697)
+skriv_artikel("Sölvesborg", 2595, 522, 322, 341, 441, 3775, 363, 2860, 220, 114, 1720)
+skriv_artikel("Umeå", 13248, 4449, 3960, 3069, 6521, 29032, 9907, 4539, 5583, 647, 10336)
+skriv_artikel("Täby", 19836, 2804, 4944, 3072, 2687, 5487, 976, 2824, 1161, 226, 4977)
+
